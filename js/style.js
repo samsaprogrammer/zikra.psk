832bce210466d9cde4a6c6d68ecaabc991b1f500
Samsa <samsa.programmer@gmail.com>
2023-07-09 10:01:03 +0530

    no change

--- js/style.js
+++ js/style.js
@@ -37,19 +37,7 @@ btn.onclick = function(){
 
 // Theme Change Section end
 
- /* page refresh section start */
 
- function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-    }
-    
-    function fadeOut(){
-        setInterval(loader, 3000);
-    }
-  
-    window.onload = fadeOut();
-  
-    /* page refresh section end */
 
 
 
